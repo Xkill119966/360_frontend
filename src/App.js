4b6b029ffeff77627be77666c4ab779a08e0c5dd
Xkill119966 <devthureinhtun@gmail.com>
2022-10-25 14:02:54 +0630

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import Navbar from "./components/navbar";
-import FormPage from "./components/FormPage";
-
-import Footer from "./components/footer";
-import LandingPage from "./components/Landing";
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <FormPage />
-      <Footer />
-    </div>
-=======
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./router/Routes";
 function App() {
@@ -21,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
->>>>>>> e488ce16e11227059fce4ab16e412506da1f28a5
   );
 }
 
